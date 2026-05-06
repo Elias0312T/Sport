ac08fc041561fbe2d611a96b08e9b5fe6f06cbc9
Elias0312T <elias.toenjes@gmail.com>
2026-05-06 10:41:48 +0200

    script.js aktualisieren

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase Config
 const firebaseConfig = {
   apiKey: "AIzaSyBUDnQCtE7PoquOGjArchkZaYZJmT7oRBY",
   authDomain: "elias-fitness-friends.firebaseapp.com",
   projectId: "elias-fitness-friends",
   storageBucket: "elias-fitness-friends.firebasestorage.app",
   messagingSenderId: "387221074867",
-  appId: "1:387221074867:web:9538df426a919c3be811c6",
-  measurementId: "G-SFNTN78BYT"
+  appId: "1:387221074867:web:9538df426a919c3be811c6"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Firebase starten
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
+// Registrierung
+function register() {
+  const email = prompt("Email:");
+  const password = prompt("Passwort:");
+
+  auth.createUserWithEmailAndPassword(email, password)
+    .then(() => alert("Account erstellt"))
+    .catch(err => alert(err.message));
+}
+
+// Login
+function login() {
+  const email = prompt("Email:");
+  const password = prompt("Passwort:");
+
+  auth.signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      document.getElementById("user").innerText =
+        "Eingeloggt: " + userCredential.user.email;
+    })
+    .catch(err => alert(err.message));
+}
+
+// Logout
+function logout() {
+  auth.signOut();
+  document.getElementById("user").innerText = "Nicht eingeloggt";
+}
