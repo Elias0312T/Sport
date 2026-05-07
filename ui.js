4599da3e921885ae1c0f4fe0cdc499ef571ccb60
Elias0312T <elias.toenjes@gmail.com>
2026-05-07 20:24:49 +0200

    ui.js aktualisieren

--- ui.js
+++ ui.js
@@ -1,1 +1,18 @@
+// NAVIGATION
 
+function showSection(sectionId) {
+
+  const sections =
+    document.querySelectorAll(".section");
+
+  sections.forEach(section => {
+
+    section.classList.add("hidden");
+
+  });
+
+  document
+    .getElementById(sectionId)
+    .classList.remove("hidden");
+
+}
