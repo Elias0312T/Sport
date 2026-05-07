131a034ab64d51b7bfc4d4718e372ada93b9e068
Elias0312T <elias.toenjes@gmail.com>
2026-05-07 20:25:08 +0200

    ranking.js aktualisieren

--- ranking.js
+++ ranking.js
@@ -1,1 +1,38 @@
+async function loadRanking() {
 
+  const rankingList =
+    document.getElementById("rankingList");
+
+  rankingList.innerHTML = "";
+
+  const snapshot =
+    await db
+      .collection("users")
+      .orderBy("xp", "desc")
+      .limit(20)
+      .get();
+
+  let rank = 1;
+
+  snapshot.forEach(doc => {
+
+    const data = doc.data();
+
+    const li =
+      document.createElement("li");
+
+    li.innerHTML = `
+      🏆 #${rank}
+      ${data.email}
+      —
+      Level ${data.level}
+      (${data.xp} XP)
+    `;
+
+    rankingList.appendChild(li);
+
+    rank++;
+
+  });
+
+}
