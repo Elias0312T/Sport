9df5c69a1783a41e1b62d4686897a672faee338d
Elias0312T <elias.toenjes@gmail.com>
2026-05-08 09:37:32 +0200

    ranking.js aktualisieren

--- ranking.js
+++ ranking.js
@@ -1,32 +1,53 @@
-async function loadRanking() {
+// GLOBAL RANKING
+
+async function loadRanking(){
 
   const rankingList =
-    document.getElementById("rankingList");
+    document.getElementById(
+      "rankingList"
+    );
 
   rankingList.innerHTML = "";
 
   const snapshot =
     await db
       .collection("users")
-      .orderBy("xp", "desc")
-      .limit(20)
+      .orderBy("xp","desc")
+      .limit(25)
       .get();
 
   let rank = 1;
 
-  snapshot.forEach(doc => {
+  snapshot.forEach(doc=>{
 
-    const data = doc.data();
+    const data =
+      doc.data();
 
     const li =
-      document.createElement("li");
+      document.createElement(
+        "li"
+      );
 
     li.innerHTML = `
-      🏆 #${rank}
-      ${data.email}
-      —
-      Level ${data.level}
-      (${data.xp} XP)
+
+      <div>
+
+        🏆 #${rank}
+
+        <br><br>
+
+        ${data.email}
+
+      </div>
+
+      <strong>
+
+        Level ${data.level}
+
+        (${data.xp} XP)
+
+      </strong>
+
     `;
 
     rankingList.appendChild(li);
